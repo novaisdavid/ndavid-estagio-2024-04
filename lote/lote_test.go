6ab1328693a46b0b2ffb327fd2396d1d1331c86c
novaisdavid <novaisdavid.19@gmail.com>
2024-05-07 14:11:49 +0100

    retorna falha da data validade próxima

--- lote/lote_test.go
+++ lote/lote_test.go
@@ -43,3 +43,34 @@ func TestMostraOsLotesComDataValidadeMaisProxima__LotesComDataValidadeMaisProxim
 	//assert
 	Validar(t, l[0].DataDeValidade, DataActual)
 }
+
+func TestFalhaAoMostraOsLotesComDataValidadeMaisProxima__Falha(t *testing.T) {
+	// arrange
+	lot := lote.Lote{}
+
+	lotes := []lote.Lote{
+
+		{IdLote: "LOTE001",
+			IdProduto:        "001",
+			DataDeProducao:   "2022-02-12",
+			DataDeValidade:   "2025-01-11",
+			NumeroDeUnidades: 20,
+			Localizacao:      "11/02/03",
+		},
+
+		{IdLote: "LOTE002",
+			IdProduto:        "001",
+			DataDeProducao:   "2022-03-12",
+			DataDeValidade:   "2025-02-11",
+			NumeroDeUnidades: 20,
+			Localizacao:      "11/02/04",
+		},
+	}
+
+	DataActual := "2025-01-12"
+	//act
+	l := lot.RetornaLoteComDataDeValidadeMaisProxima(lotes, DataActual)
+
+	//assert
+	Validar(t, l[0].DataDeValidade, DataActual)
+}

9456f4a8e8ab02c20c654565cd8b43144b916af7
novaisdavid <novaisdavid.19@gmail.com>
2024-05-08 14:56:28 +0100

    Focar nos lotes

--- produto_stock_test.go
+++ produto_stock_test.go
@@ -11,6 +11,12 @@ func Validar(t *testing.T, valorEsperado produto_stock.Produto) {
 		t.Fail()
 	}
 }
+func ValidarSeNaoExiste(t *testing.T, valorEsperado produto_stock.Produto) {
+	if valorEsperado.IdProduto != "" {
+		t.Log("Falha")
+		t.Fail()
+	}
+}
 
 func TestVerificaSeProdutoExiste_Produto(t *testing.T) {
 	//arrange
@@ -36,4 +42,30 @@ func TestVerificaSeProdutoExiste_Produto(t *testing.T) {
 	produtoEncontrado := produto1.VerificaSeProdutoExiste(produtos, idProduto)
 	//Assert
 	Validar(t, produtoEncontrado)
+}
+
+func TestVerificaSeProdutoNaoExiste_Nada(t *testing.T) {
+	//arrange
+	produto1 := produto_stock.Produto{}
+	produtos := []produto_stock.Produto{
+		{
+			IdProduto:      "001",
+			NomeDoProduto:  "CocaColaEmLata330ml",
+			Categoria:      "Bebidas",
+			DataDeProducao: "2022-05-01",
+			DataDeValidade: "2025-05-01",
+		},
+		{
+			IdProduto:      "002",
+			NomeDoProduto:  "SpriteEmLata330ml",
+			Categoria:      "Bebidas",
+			DataDeProducao: "2022-06-01",
+			DataDeValidade: "2025-06-01",
+		},
+	}
+	idProduto := ""
+	//act
+	produtoEncontrado := produto1.VerificaSeProdutoExiste(produtos, idProduto)
+	//Assert
+	ValidarSeNaoExiste(t, produtoEncontrado)
 }

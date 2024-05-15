f58b24317b348eb3ebe899f56973d915b5f9ded1
novaisdavid <novaisdavid.19@gmail.com>
2024-05-15 14:26:43 +0100

    matricla formando

--- firma/Firmacao/servico/servico.go
+++ firma/Firmacao/servico/servico.go
@@ -4,27 +4,66 @@ import (
 	curso "Firma/curso"
 	formando "Firma/formando"
 	matricula "Firma/matricula"
+	"fmt"
 	"math/rand"
 	"strconv"
 	"time"
 )
 
-
 func FazerMatriculadeFormando(dados ...interface{}) {
+
 	rand.Seed(time.Now().UnixNano())
 	idEstudante := rand.Intn(100)
 	f := formando.Formando{}
 	c := curso.Curso{}
 	m := matricula.Matricula{}
-
 	id := strconv.Itoa(idEstudante)
 
-	if len(dados) == 5 {
-		f.New(id, dados[0].(string), dados[1].(string), dados[2].(string))
-		c.New("c001", dados[3].(string), "1-introduçao ao CCTV", 54, dados[4].(string))
+	if len(dados) < 5 {
+		fmt.Println("Por favor preenchas dos campos correctamente")
+		return
 	}
 
-	m.New(f.GetIdFormando(), c.GetIdCurso())
+	f.New(id, dados[0].(string), dados[1].(string), dados[2].(string))
+	curso := c.BuscaUmCursoPorNomeRegime(dados[3].(string), dados[4].(string))
+	if curso.GetIdCurso() == "" {
+		fmt.Println("")
+		fmt.Println("CURSO NÂO ENCONTRADO! FORMANDO NÂO MATRICULADO")
+		return
+	}
+
+	fmt.Println("")
+	fmt.Println("NOME: ", f.GetNomeFormando())
+	fmt.Println("")
+	fmt.Println("====== DADOS DO CURSO ======")
+	fmt.Println("CURSO: ", curso.GetNome())
+	fmt.Println("CARGA HORÁRIA: ", strconv.Itoa(curso.GetHora()))
+	fmt.Println("REGIME: ", curso.GetRegime())
+
+	m.New(f.GetIdFormando(), curso.GetIdCurso())
 
 	m.Salvar()
+}
+
+func CadastrarCursos(dados ...interface{}) {
+	rand.Seed(time.Now().UnixNano())
+	idCurso := rand.Intn(100)
+	c := curso.Curso{}
+
+	id := strconv.Itoa(idCurso)
+
+	if len(dados) >= 4 {
+
+		c.New(id, dados[0].(string), dados[1].(string), dados[2].(int), dados[3].(string))
+	}
+
+	fmt.Println("")
+	fmt.Println("CURSO: ", c.GetNome())
+	fmt.Println("CARGA HORÁRIA: ", c.GetHora())
+	fmt.Println("REGIME: ", c.GetRegime())
+	fmt.Println("CONTEUDO PROGRAMÁTICO: ", c.GetConteudoProgramatico())
+	c.Salvar()
+	fmt.Println("")
+	fmt.Println("CURSO CADASTRADO COM SUCESSO!")
+
 }
